6079c4480e75a6d5ae04ae075c6df134435b535d
Nidhi Dhote <nidhidhote512@gmail.com>
2023-07-26 14:09:35 +0530

    SpringBoot Hello World

---
+++ springboot/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String helloToTheWorld(){
+        return "Greetings to the world";
+    }
+}
